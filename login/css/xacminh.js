20b16ffeca2c2e14ef545f426da9024d0917c092
KunnAOV <killervn08042006@gmail.com>
2024-11-20 14:02:39 +0700

    Thêm file login/css/xacminh.js

---
+++ login/css/xacminh.js
@@ -0,0 +1,18 @@
+ var captchaGenerate = function() {
+        var b = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(a) {
+            var b = 16 * Math.random() | 0;
+            return ("x" == a ? b : b & 3 | 8).toString(16)
+        }).replace(/-/g, "");
+        $("#captcha-key").val(b);
+        $("#captcha-img").attr("src", "captcha.php?id=" + b)
+    },
+    refreshCaptcha = function() {
+        $(".refresh").click(function() {
+            captchaGenerate()
+        })
+    };
+$("#sso_captcha_wrap").show(), $("#input-captcha").attr("required", !0);
+$(document).ready(function() {
+    captchaGenerate();
+    refreshCaptcha()
+});

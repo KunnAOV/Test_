f642e5e9023899077bca0b699f3fcc3c059c08fe
KunnAOV <killervn08042006@gmail.com>
2024-11-20 14:02:21 +0700

    Thêm file Include_LXT/Login.php

---
+++ Include_LXT/Login.php
@@ -0,0 +1,81 @@
+<div class="ReactModalPortal">
+   <div class="ReactModal__Overlay ReactModal__Overlay--after-open" style="position: fixed; inset: 0px; background-color: rgba(255, 255, 255, 0.75);">
+      <div class="ReactModal__Content ReactModal__Content--after-open modal-select modal-register-info" tabindex="-1" role="dialog">
+         <div class="modal-description modal-register-info">
+            <div class="modal-header">
+               <h2 class="modal-title"><img src="/ThemeLXT/Images/icon-register.png" alt="Đăng ký thông tin">ĐĂNG NHẬP ĐỂ THAM GIA CỘNG ĐỒNG    </h2>
+            </div>
+            <div class="modal-body">
+               <div class="ScrollbarsCustom trackYVisible" style="position: relative; width: 100%; height: 52vh;">
+                  <div class="ScrollbarsCustom-Wrapper" style="position: absolute; inset: 0px 10px 0px 0px; overflow: hidden;">
+                     <div class="ScrollbarsCustom-Scroller" style="position: absolute; inset: 0px; overflow: hidden scroll; padding-right: 20px; margin-right: -21px;">
+                        <div class="ScrollbarsCustom-Content" style="box-sizing: border-box; padding: 0.05px; min-height: 100%; min-width: 100%;">
+                           <div class="modal-content">
+                              <form class="form-register-info">
+                     
+
+
+<style type="text/css">
+    .login-center {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 50%;
+    margin: 50px auto;
+  }
+
+  .login-center img {
+    width: 60px;
+    height: 60px;
+  }
+</style>
+
+                             <p class="not-log" style="color: #fff;font-size: 14px;text-align:center;">Vui lòng đăng nhập để đăng ký tham gia sự kiện cộng đồng</p>
+    
+                             
+
+
+
+                             <div class="login-center">
+                                <div class="fb">
+                                    <a href="?FbLoginLXT#fb">
+                                        <img src="ThemeLXT/Images/logo-facebook.png" alt="Garena">
+                                    </a>
+                                </div>
+                                <div class="gas">
+                                    <a href="login">
+                                        <img src="ThemeLXT/Images/logo-garena.png" alt="Facebook">
+                                    </a>
+                                </div>
+                            </div>
+                          
+                                     
+
+
+
+
+                                 <button type="submit" class="d-none" disabled="">&nbsp;</button>
+                              </form>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="ScrollbarsCustom-Track ScrollbarsCustom-TrackY" style="position: absolute; overflow: hidden; border-radius: 4px; background: rgba(0, 0, 0, 0.1); user-select: none; width: 10px; height: calc(100% - 20px); top: 10px; right: 0px;">
+                     <div class="ScrollbarsCustom-Thumb ScrollbarsCustom-ThumbY" style="cursor: pointer; border-radius: 4px; background: rgba(0, 0, 0, 0.4); width: 100%; height: 258.918px; transform: translateY(0px);"></div>
+                  </div>
+                  <div class="ScrollbarsCustom-Track ScrollbarsCustom-TrackX" style="position: absolute; overflow: hidden; border-radius: 4px; background: rgba(0, 0, 0, 0.1); user-select: none; height: 10px; width: calc(100% - 20px); bottom: 0px; left: 10px; display: none;">
+                     <div class="ScrollbarsCustom-Thumb ScrollbarsCustom-ThumbX" style="cursor: pointer; border-radius: 4px; background: rgba(0, 0, 0, 0.4); height: 100%; width: 0px; display: none;"></div>
+                  </div>
+               </div>
+            </div>
+            <div class="modal-footer">
+               <div class="form-action">
+                  <div class="custom-control">Bản quyền thuộc Garena Center</div>
+                  
+               </div>
+            </div>
+         </div>
+         <a href="index.php" class="close" data-dismiss="modal" aria-label="Close" title="Đóng">×</a>
+      </div>
+   </div>
+</div>

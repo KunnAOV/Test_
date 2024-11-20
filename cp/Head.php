f875328470b14958f632997d5de97b549731599d
KunnAOV <killervn08042006@gmail.com>
2024-11-20 14:01:00 +0700

    Thêm file cp/Head.php

---
+++ cp/Head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+   <head>
+    <meta charset="utf-8">
+    <title> Hệ thống quản lý bất động sản Việt Nam </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../cp/theme/sweetalert2.css" id="theme-styles">
+    
+     </head>
+    <body>
+        <script src="../cp/theme/sweetalert2.all.min.js"></script>
